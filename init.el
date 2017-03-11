ef39537b3cddc03c4811974229526e5525ec1744
wtnbass <bass.ksk@gmail.com>
2017-03-11 17:27:44 +0900

    Add basic settings, and Comments

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-(package-initialize)
+;; (package-initialize)
 (load (setq custom-file (expand-file-name "custom.el" user-emacs-directory)))
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
@@ -9,24 +9,45 @@
 (load-theme 'monokai t)
 (nyan-mode t)
 
+;; setting key bind
 (global-unset-key (kbd "C-z"))
 (define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
+(when (eq system-type 'darwin)
+  (setq ns-command-modifier (quote meta)))
 
 (require 'cl)
 (prefer-coding-system 'utf-8)
 (fset 'yes-or-no-p 'y-or-n-p)
+
+;; hide tool bar
 (tool-bar-mode -1)
+
+;; hide scroll bar
 (set-scroll-bar-mode nil)
+
+;; set mouse scroll step
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-follow-mouse 't)
 (setq scroll-step 1)
 
+;; don't make backup files
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
 (setq transient-mark-mode t)
 (setq ring-bell-function 'ignore)
 
+;; show paren pair
+(show-paren-mode t)
+
+;; show line number
+(global-linum-mode t)
+
+;; show whitespaces
 (global-whitespace-mode t)
 (add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; tab
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 2)
