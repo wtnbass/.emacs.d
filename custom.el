b6079fa4ff92d7029cdd6de1f08dd2adae37f319
wtnbass <bass.ksk@gmail.com>
2017-03-11 17:31:34 +0900

    Add custom.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-mode yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors monokai-theme magit json-mode idle-highlight-mode htmlize helm-swoop flymake-go flycheck-cask expand-region exec-path-from-shell emmet-mode drag-stuff company-tern company-go anzu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
